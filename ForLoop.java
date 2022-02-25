e806ab8f6b31de9943e3143b1a9cf6a4a2049d6d
Josiah Cury <josiah.cury@student.nmt.edu>
2022-02-25 15:10:08 -0700

    Added working ForLoop.Java, HelloWorld.java, IntArith.java, WhileLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,11 @@
+public class hello {
+    int a;
+    int i;
+   public static void main(String argv[]) {
+      a = 15;
+      for(i = 1; i < a; ){
+          System.out.println("juicy\t");
+      }
+
+   }
+}
---
+++ IntArith.java
@@ -0,0 +1,8 @@
+public class hello {
+    int a;
+    int i;
+   public static void main(String argv[]) {
+      a = 15;
+      i = a * 4;
+   }
+}
